b579bc12a5b8471035348680288e7d51fab8a355
valeraqwe <valera.komar.1999@gmail.com>
2023-10-05 13:32:53 +0300

    Implemented CRUD methods in PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Collection
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $post = Post::create($request->all());
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post): Post
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post): JsonResponse
+    {
+        $post->update($request->all());
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post): JsonResponse
+    {
+        $post->delete();
+        return response()->json(null, 204);
+    }
+
+}
