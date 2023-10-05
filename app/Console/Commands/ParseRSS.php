a435ec2c47b2b92514a624566f29b56e7867fd51
valeraqwe <valera.komar.1999@gmail.com>
2023-10-05 13:24:26 +0300

    Added Artisan command for RSS parsing

---
+++ app/Console/Commands/ParseRSS.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ParseRSS extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rss:pars';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parse RSS Feed';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
